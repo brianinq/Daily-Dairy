35ba7becae7cb29c1bbb9ee0e62f84967fec6494
brian <kipkoechbrian02gmail.com>
2022-07-17 23:33:54 +0300

    add leap year report generation functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,3 +59,43 @@ changePrice.addEventListener("click", () => {
   price.textContent = sellingPrice;
   totalProduction(shedALitres, shedBLitres, shedCLitres, shedDLitres);
 });
+function getYearlyStats(daySum, sellingPrice) {
+  const monthlyProduction = {
+    jan: [daySum * 31, daySum * 31 * sellingPrice],
+    feb: [daySum * 28, daySum * 28 * sellingPrice],
+    mar: [daySum * 31, daySum * 31 * sellingPrice],
+    apr: [daySum * 30, daySum * 30 * sellingPrice],
+    may: [daySum * 31, daySum * 31 * sellingPrice],
+    jun: [daySum * 30, daySum * 30 * sellingPrice],
+    july: [daySum * 31, daySum * 31 * sellingPrice],
+    aug: [daySum * 31, daySum * 31 * sellingPrice],
+    sept: [daySum * 30, daySum * 30 * sellingPrice],
+    oct: [daySum * 31, daySum * 31 * sellingPrice],
+    nov: [daySum * 30, daySum * 30 * sellingPrice],
+    dec: [daySum * 31, daySum * 31 * sellingPrice],
+  };
+  let jan = document.querySelectorAll(".jan");
+  [jan[0].textContent, jan[1].textContent] = monthlyProduction["jan"];
+  let feb = document.querySelectorAll(".feb");
+  [feb[0].textContent, feb[1].textContent] = monthlyProduction["feb"];
+  let mar = document.querySelectorAll(".mar");
+  [mar[0].textContent, mar[1].textContent] = monthlyProduction["mar"];
+  let apr = document.querySelectorAll(".apr");
+  [apr[0].textContent, apr[1].textContent] = monthlyProduction["apr"];
+  let may = document.querySelectorAll(".may");
+  [may[0].textContent, may[1].textContent] = monthlyProduction["may"];
+  let jun = document.querySelectorAll(".jun");
+  [jun[0].textContent, jun[1].textContent] = monthlyProduction["jun"];
+  let july = document.querySelectorAll(".july");
+  [july[0].textContent, july[1].textContent] = monthlyProduction["july"];
+  let aug = document.querySelectorAll(".aug");
+  [aug[0].textContent, aug[1].textContent] = monthlyProduction["aug"];
+  let sept = document.querySelectorAll(".sept");
+  [sept[0].textContent, sept[1].textContent] = monthlyProduction["sept"];
+  let oct = document.querySelectorAll(".oct");
+  [oct[0].textContent, oct[1].textContent] = monthlyProduction["oct"];
+  let nov = document.querySelectorAll(".nov");
+  [nov[0].textContent, nov[1].textContent] = monthlyProduction["nov"];
+  let dec = document.querySelectorAll(".dec");
+  [dec[0].textContent, dec[1].textContent] = monthlyProduction["dec"];
+}
