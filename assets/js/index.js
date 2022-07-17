3c3ea7f95a329af3ecfd9013d64812fafa4fd47c
brian <kipkoechbrian02gmail.com>
2022-07-17 23:30:23 +0300

    Add income total production calculation function

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,21 @@ const DAYS_OF_WEEK = 7;
 const DAYS_IN_YEAR = 365;
 let sellingPrice = 45;
 let daySum = 0;
+
+function totalProduction(shedA, shedB, shedC, shedD) {
+  Array.from(dailyTable)
+    .slice(0, dailyTable.length - 1)
+    .forEach((element, index) => {
+      element.textContent = arguments[index];
+      daySum += Number(arguments[index]);
+    });
+  dailyTable[dailyTable.length - 1].textContent = daySum;
+  weeklyProduction(shedA, shedB, shedC, shedD, daySum);
+}
+function weeklyProduction(shedA, shedB, shedC, shedD, daysTotal) {
+  weeklyTable.forEach((element, index) => {
+    element.textContent = Number(arguments[index]) * DAYS_OF_WEEK;
+  });
+  incomeOverTime(sellingPrice, daysTotal);
+  getYearlyStats(daySum, sellingPrice);
+}
